0083d9cc9b247c3d10c5a4fddc8f55108c78f634
Bryan Hansen <bry.hansen@aggiemail.usu.edu>
2012-02-11 23:10:57 -0700

    Added setting motor speeds via joystick and subscription to depth sensor.

--- SubConsole/src/SubConsole.cpp
+++ SubConsole/src/SubConsole.cpp
@@ -1,5 +1,6 @@
 #include <QPoint>
 #include<QDesktopWidget>
+#include <math.h>
 
 #include "SubConsole.hpp"
 #include "ui_SubConsole.h"
@@ -15,17 +16,21 @@ SubConsole::SubConsole(QWidget* pParent)
      m_pJoystickTimer(new QTimer(this)),
      m_pJoystick(new Joystick()),
      m_motorPublisher(),
-     m_missionTogglePublisher(),
      m_imuSubscriber(),
      m_motorControllerTempSubscriber(),
-     m_motorCaseTempSubscriber()
+     m_motorCaseTempSubscriber(),
+     m_pressureSubscriber(),
+     m_motorStateSubscriber(),
+     m_lastXAxisValue(0),
+     m_lastYAxisValue(0),
+     m_lastThrottleValue(0),
+     m_lastTwistValue(0)
 {
    m_pUi->setupUi(this);
    m_pJoystickTimer->setInterval(JOYSTICK_POLL_INTERVAL_MSEC);
 
    connect(m_pJoystickTimer, SIGNAL(timeout()), this, SLOT(readJoystickInput()));
    connect(m_pUi->connectButton, SIGNAL(clicked()), this, SLOT(joyConnect()));
-   connect(m_pUi->missionToggleSlider, SIGNAL(valueChanged(int)), this, SLOT(missionToggle()));
 
    //Attempt to connect to default joystick location
    joyConnect();
@@ -35,13 +40,13 @@ SubConsole::SubConsole(QWidget* pParent)
 
    ros::NodeHandle nodeHandle;
 
-   //@todo create a custom message for the Motor_Driver topic
-  //m_motorPublisher = nodeHandle.advertise<std_msgs::String>("Motor_Driver", 100);
-   m_missionTogglePublisher = nodeHandle.advertise<std_msgs::Bool>("Motor_State", 100);
+   m_motorPublisher = nodeHandle.advertise<Ui::motorMsg>("Motor_Driver", 100);
 
    m_imuSubscriber = nodeHandle.subscribe("IMU_Data", 100, &SubConsole::imuDataCallback, this);
    m_motorControllerTempSubscriber = nodeHandle.subscribe("Motor_Controller_Temp", 100, &SubConsole::motorControllerTempCallback, this);
    m_motorCaseTempSubscriber = nodeHandle.subscribe("Motor_Case_Temp", 100, &SubConsole::motorCaseTempCallback, this);
+   m_pressureSubscriber = nodeHandle.subscribe("Pressure_Data", 100, &SubConsole::pressureDataCallback, this);
+   m_motorStateSubscriber = nodeHandle.subscribe("Motor_State", 100, &SubConsole::motorStateCallback, this);
 }
 
 /**
@@ -79,35 +84,93 @@ void SubConsole::joyConnect(void)
  **/
 void SubConsole::readJoystickInput(void)
 {
-   //@todo store last state of each axis, create and send message if changed for each changed axis
+   int currentXAxis = m_pJoystick->getAxis(0);
+   int currentYAxis = m_pJoystick->getAxis(1);
+   int currentTwistAxis = m_pJoystick->getAxis(2);
+   int currentThrottleAxis = m_pJoystick->getAxis(3);
 
-   //m_pJoystick->getAxis(0); //x-axis
-   //m_pJoystick->getAxis(1); //y-axis
-   //m_pJoystick->getAxis(2); //twist
-   //m_pJoystick->getAxis(3); //throttle
-   //m_pJoystick->getAxis(4); //nub x-axis
+   if(m_lastXAxisValue != currentXAxis)
+   {
+       //Set the horizontal thrusters to opposite thrust to strafe
+       int frontTurnSpeed = 127 * (abs(currentXAxis) / (double)JOYSTICK_MAX_VALUE);
+       int backTurnSpeed = 127 * (abs(currentXAxis) / (double)JOYSTICK_MAX_VALUE);
 
-   //m_pJoystick->getButton(0); //trigger
+       if(currentXAxis >= 0)
+       {
+          backTurnSpeed = frontTurnSpeed + 128;
+       }
+       else
+       {
+          frontTurnSpeed = backTurnSpeed + 128;
+       }
+
+       sendMotorSpeedMsg(MOTOR_FRONT_TURN, frontTurnSpeed);
+       sendMotorSpeedMsg(MOTOR_BACK_TURN, backTurnSpeed);
+
+       m_lastXAxisValue = currentXAxis;
+   }
+
+   if(m_lastYAxisValue != currentYAxis)
+   {
+      //Set the forward/reverse thrusters to same value to move forwards or backwards
+      int thrusterSpeed = 127 * (abs(currentYAxis) / (double)JOYSTICK_MAX_VALUE);
+
+      //A neg number means the stick is pushed forward, if positive we actually want reverse
+      if(currentYAxis >= 0)
+      {
+         thrusterSpeed += 128;
+      }
+
+      sendMotorSpeedMsg(MOTOR_LEFT_THRUST | MOTOR_RIGHT_THRUST, thrusterSpeed);
+
+      m_lastYAxisValue = currentYAxis;
+   }
+
+   if(m_lastTwistValue != currentTwistAxis)
+   {
+      //Set the horizontal thrusters to the same direction/velocity to rotate sub
+      int thrusterSpeed = 127 * (abs(currentTwistAxis) / (double)JOYSTICK_MAX_VALUE);
+
+      if(currentTwistAxis < 0)
+      {
+         thrusterSpeed += 128;
+      }
+
+      sendMotorSpeedMsg(MOTOR_FRONT_TURN | MOTOR_BACK_TURN, thrusterSpeed);
+
+      m_lastTwistValue = currentTwistAxis;
+   }
+
+   if(m_lastThrottleValue != currentThrottleAxis)
+   {
+      //Set the vertical thrusters to the same value to control depth
+      int thrusterSpeed = 127 * (abs(currentThrottleAxis) / (double)JOYSTICK_MAX_VALUE);
+
+      if(currentThrottleAxis < 0)
+      {
+         thrusterSpeed += 128;
+      }
+
+      sendMotorSpeedMsg(MOTOR_FRONT_DEPTH | MOTOR_BACK_DEPTH, thrusterSpeed);
+
+      m_lastThrottleValue = currentThrottleAxis;
+   }
 }
 
 /**
- * @brief Called when the mission toggle slider is changed
+ * @brief Publishes a Motor_Driver message
  *
- * @param value The new slider value
+ * @param motorMask Bit mask with the motors to drive at the specified speed
  **/
-void SubConsole::missionToggle(int value)
+void SubConsole::sendMotorSpeedMsg(unsigned char motorMask, unsigned char motorSpeed)
 {
-   std_msgs::Bool msg;
-   bool toggle = true;
+   Ui::motorMsg motorMsg;
 
-   if(value != 0)
-   {
-      toggle = false;
-   }
+   motorMsg.motorMask = motorMask;
+   motorMsg.speed = motorSpeed;
 
-   msg.data = toggle;
+   m_motorPublisher.publish(motorMsg);
 
-   m_missionTogglePublisher.publish(msg);
 }
 
 /**
@@ -115,9 +178,11 @@ void SubConsole::missionToggle(int value)
  *
  * @param msg The received message
  **/
-void SubConsole::imuDataCallback(const std_msgs::String::ConstPtr& msg)
+void SubConsole::imuDataCallback(const Ui::imuMsg::ConstPtr& msg)
 {
-   //@todo pull out roll, pitch, and yaw once an actual message is defined
+   m_pUi->yawLineEdit->setText(QString::number(msg->yaw));
+   m_pUi->pitchLineEdit->setText(QString::number(msg->pitch));
+   m_pUi->rollLineEdit->setText(QString::number(msg->roll));
 }
 
 /**
@@ -142,4 +207,54 @@ void SubConsole::motorCaseTempCallback(const std_msgs::Float32::ConstPtr& msg)
    float temperature = msg->data;
 
    m_pUi->motorCaseTempLineEdit->setText(QString::number(temperature));
+}
+
+/**
+ * @brief ROS callback for Pressure_Data subscription
+ *
+ * @param msg The received message
+ **/
+void SubConsole::pressureDataCallback(const std_msgs::Float32::ConstPtr& msg)
+{
+   float pressure = msg->data;
+
+   m_pUi->pressureLineEdit->setText(QString::number(pressure));
+}
+
+/**
+ * @brief ROS callback for Motor_State subscription
+ *
+ * @param msg The received message
+ **/
+void SubConsole::motorStateCallback(const std_msgs::Bool::ConstPtr& msg)
+{
+   bool motorEnabled = msg->data;
+
+   if(motorEnabled)
+   {
+      m_pUi->motorStateLineEdit->setText("Enabled");
+   }
+   else
+   {
+      m_pUi->motorStateLineEdit->setText("Disabled");
+   }
+}
+
+/**
+ * @brief ROS callback for Mission_State subscription
+ *
+ * @param msg The received message
+ **/
+void SubConsole::missionStateCallback(const std_msgs::Bool::ConstPtr& msg)
+{
+   bool missionEnabled = msg->data;
+
+   if(missionEnabled)
+   {
+      m_pUi->missionStateLineEdit->setText("Enabled");
+   }
+   else
+   {
+      m_pUi->missionStateLineEdit->setText("Disabled");
+   }
 }
--- SubConsole/src/SubConsole.hpp
+++ SubConsole/src/SubConsole.hpp
@@ -9,6 +9,8 @@
 #include "std_msgs/String.h"
 #include "std_msgs/Bool.h"
 #include "std_msgs/Float32.h"
+#include "../msg/imuMsg.h"
+#include "../msg/motorMsg.h"
 
 namespace Ui
 {
@@ -27,24 +29,41 @@ private:
    QTimer* m_pJoystickTimer;
    Joystick* m_pJoystick;
    ros::Publisher m_motorPublisher;
-   ros::Publisher m_missionTogglePublisher;
    ros::Subscriber m_imuSubscriber;
    ros::Subscriber m_motorControllerTempSubscriber;
    ros::Subscriber m_motorCaseTempSubscriber;
+   ros::Subscriber m_pressureSubscriber;
+   ros::Subscriber m_motorStateSubscriber;
+   ros::Subscriber m_missionStateSubscriber;
+
+   int m_lastXAxisValue;
+   int m_lastYAxisValue;
+   int m_lastThrottleValue;
+   int m_lastTwistValue;
 
    enum
    {
-      JOYSTICK_POLL_INTERVAL_MSEC = 100
+      JOYSTICK_POLL_INTERVAL_MSEC = 100,
+      JOYSTICK_MAX_VALUE = 32767,
+      MOTOR_FRONT_TURN = 128,
+      MOTOR_BACK_TURN = 64,
+      MOTOR_FRONT_DEPTH = 32,
+      MOTOR_BACK_DEPTH = 16,
+      MOTOR_LEFT_THRUST = 8,
+      MOTOR_RIGHT_THRUST = 4
    };
 
-   void imuDataCallback(const std_msgs::String::ConstPtr& msg);
+   void imuDataCallback(const Ui::imuMsg::ConstPtr& msg);
    void motorControllerTempCallback(const std_msgs::Float32::ConstPtr& msg);
    void motorCaseTempCallback(const std_msgs::Float32::ConstPtr& msg);
+   void pressureDataCallback(const std_msgs::Float32::ConstPtr& msg);
+   void motorStateCallback(const std_msgs::Bool::ConstPtr& msg);
+   void missionStateCallback(const std_msgs::Bool::ConstPtr& msg);
+   void sendMotorSpeedMsg(unsigned char motorMask, unsigned char motorSpeed);
 
 private slots:
    void readJoystickInput(void);
    void joyConnect(void);
-   void missionToggle(int value);
 };
 
 #endif // SUBCONSOLE_HPP
