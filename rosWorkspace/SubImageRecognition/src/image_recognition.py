da43613884c14e1d197abfc0e1074cfc36010037
robosub <robosub@gilligan.(none)>
2012-03-08 13:54:28 -0700

    Lots of changes to downward camera stuff in SubImageRecognition

--- rosWorkspace/SubImageRecognition/src/image_recognition.py
+++ rosWorkspace/SubImageRecognition/src/image_recognition.py
@@ -5,9 +5,11 @@ roslib.load_manifest("SubImageRecognition")
 import sys
 import rospy
 import math
+import time
 import cv
 from std_msgs.msg import String
 from sensor_msgs.msg import Image
+from SubImageRecognition.msg import OrangeRectangle
 from cv_bridge import CvBridge, CvBridgeError
 
 
@@ -20,7 +22,7 @@ class ImageRecognition:
         self._forward_sub = rospy.Subscriber(
                  "left/image_raw", Image, self.forward_callback)
         
-        #self._downward_data_pub = rospy.Publisher("downward_camera/data", TODO)
+        self._downward_rect_pub = rospy.Publisher("downward_camera/orange_rectangle", OrangeRectangle)
         self._downward_img_pub = rospy.Publisher("downward_camera/image_raw", Image)
         self._downward_sub = rospy.Subscriber(
                 "right/image_raw", Image, self.downward_callback)
@@ -34,7 +36,7 @@ class ImageRecognition:
         
         # Rotate so that 'up' is the top of the sub
         rotated = cv.CreateImage((image_raw.height, image_raw.width), cv.IPL_DEPTH_8U, 3)
-        self.rotate_image_ccw(image_raw, rotated)
+        self.rotate_image_cw(image_raw, rotated)
         
         # TODO: Right now this callback just runs 'good features to track'
         #       algorithm against a grayscale version of the rotated image.
@@ -86,7 +88,7 @@ class ImageRecognition:
         
         # Perform threshold to isolate a range of colors
         threshold = cv.CreateImage(size, cv.IPL_DEPTH_8U, 1)
-        cv.InRangeS(segmented, (0, 50, 50), (20, 255, 255), threshold) # XXX: temporarily changed min hue from 5 to 0
+        cv.InRangeS(segmented, (5, 50, 50), (15, 255, 255), threshold)
         
         # Reduce noise in threshold using erode and dilate
         element = cv.CreateStructuringElementEx(3, 3, 1, 1, cv.CV_SHAPE_ELLIPSE)
@@ -97,35 +99,51 @@ class ImageRecognition:
         cv.Dilate(threshold, threshold, element, 4)
         
         # Sample threshold for white points
+        # TODO: Segment points into grouped regions
         points = []
         for i in range(0, size[1], 13): # height
             for j in range(0, size[0], 13): # width
                 if cv.Get2D(threshold, i, j)[0] == 255.0:
                     points.append((j, i))
         
-        # Throw away outlier points
-        #TODO
+        # Throw away bad groups
+        # TODO: Do this for each of the grouped regions
+        if len(points) < 5:
+            points = None
         
         # Find minimum area rotated rectangle around white points
-        (center, dims, rotation) = cv.MinAreaRect2(points)
-        rotation *= math.pi / 180 # Convert rotation from degrees to radians
-        
-        # Correct dims/rotation so that the first dim is always larger
-        #TODO
-        
-        # Mark rectangle corners on rotated image
-        v0 = (dims[0]/2*math.cos(rotation), dims[0]/2*math.sin(rotation))
-        v1 = (dims[1]/2*math.cos(rotation+math.pi/2), dims[1]/2*math.sin(rotation+math.pi/2))
-        cv.Circle(rotated, (int(center[0] + v0[0] + v1[0]), int(center[1] + v0[1] + v1[1])), 1 , (0, 0, 255), 5)
-        cv.Circle(rotated, (int(center[0] + v0[0] - v1[0]), int(center[1] + v0[1] - v1[1])), 1 , (0, 0, 255), 5)
-        cv.Circle(rotated, (int(center[0] - v0[0] + v1[0]), int(center[1] - v0[1] + v1[1])), 1 , (0, 0, 255), 5)
-        cv.Circle(rotated, (int(center[0] - v0[0] - v1[0]), int(center[1] - v0[1] - v1[1])), 1 , (0, 0, 255), 5)
-        
-        # Compute rectangle confidence level based on previous data
-        #TODO
-        
-        # Publish rectangle and confidence level data
-        #TODO
+        #if points:
+        if points:
+            (center, dims, rotation) = cv.MinAreaRect2(points)
+            rotation *= math.pi / 180 # Convert rotation from degrees to radians
+            
+            # Correct dims/rotation so that the first dim is always larger
+            if dims[0] < dims[1]:
+                dims = (dims[1], dims[0])
+                rotation += math.pi / 2
+            
+            # Normalize rotation for drawing
+            if math.sin(rotation) > 0:
+                rotation += math.pi
+            rotation %= math.pi * 2
+            
+            # Mark rectangle on rotated image
+            v0 = (dims[0]/2*math.cos(rotation), dims[0]/2*math.sin(rotation))
+            v1 = (dims[1]/2*math.cos(rotation+math.pi/2), dims[1]/2*math.sin(rotation+math.pi/2))
+            cv.Circle(rotated, (int(center[0]), int(center[1])), 1 , (0, 0, 255), 5)
+            cv.Line(rotated, (int(center[0]), int(center[1])), (int(center[0] + v0[0]), int(center[1] + v0[1])), (0, 0, 255))
+            cv.Circle(rotated, (int(center[0] + v0[0] + v1[0]), int(center[1] + v0[1] + v1[1])), 1 , (0, 0, 255), 5)
+            cv.Circle(rotated, (int(center[0] + v0[0] - v1[0]), int(center[1] + v0[1] - v1[1])), 1 , (0, 0, 255), 5)
+            cv.Circle(rotated, (int(center[0] - v0[0] + v1[0]), int(center[1] - v0[1] + v1[1])), 1 , (0, 0, 255), 5)
+            cv.Circle(rotated, (int(center[0] - v0[0] - v1[0]), int(center[1] - v0[1] - v1[1])), 1 , (0, 0, 255), 5)
+            
+            # Normalize rotation for publishing
+            if rotation == 0:
+                rotation = math.pi * 2
+            rotation -= 3 * math.pi / 2
+            
+            # Publish rectangle data
+            self._downward_rect_pub.publish(OrangeRectangle(stamp=roslib.rostime.Time(time.time()), center_x=int(center[0]), center_y=int(center[1]), rotation=rotation))
         
         # Publish image
         try:
