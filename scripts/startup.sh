d6f9f3781eab7e8d59b9c9f308a0237995749149
= <subcrew@Subcrew.(none)>
2012-03-28 13:12:06 -0600

    added stuff to the startup.sh script

--- scripts/startup.sh
+++ scripts/startup.sh
@@ -1,13 +1,27 @@
 #!/bin/bash
 
+##Stage 1
 roscore &
 
 sleep 2
 
+##Stage 2
+
 # cameras driver
 roslaunch SubCameraDriver cameras.launch &
 
+# start the sensor board
+rosrun rosserial_python serial_node.py /dev/controller_sensor &
+
+# start the imu
+rosrun SubImuController SubImuController /dev/controller_Imu &
+
+# start the motor controllers
+rosrun SubMotorController SubMotorController &
+
 sleep 1
+
+##Stage 3
 
 # republish cameras as compressed for recording
 /opt/ros/diamondback/stacks/image_common/image_transport/bin/republish raw in:=left/image_raw compressed out:=left/image_compressed &
@@ -17,6 +31,8 @@ sleep 1
 /opt/robosub/rosWorkspace/SubImageRecognition/src/image_recognition.py &
 
 sleep 1
+
+##Stage 4
 
 # republish image recognition as compressed for viewing remotely
 /opt/ros/diamondback/stacks/image_common/image_transport/bin/republish raw in:=forward_camera/image_raw compressed out:=forward_camera/image_compressed &
