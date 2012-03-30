45fa84a329571bac6d656bb078640e693dea1c38
Daniel Morwood <danmorwood@gmail.com>
2012-03-29 23:06:54 -0600

    added calibration to depth translator

--- rosWorkspace/SubTranslators/src/Depth.cpp
+++ rosWorkspace/SubTranslators/src/Depth.cpp
@@ -2,22 +2,34 @@
 #include "std_msgs/Float32.h"
 
 float offset = 83;
-float scalingFactor = .2; //Ticks per foot
+float scalingFactor = 0.2; //feet per tick
+float currentDepthRaw = 16.6;
+float lastTicks = 83;
 
 ros::Publisher depthPub;
 
 void tickMeasureCallback(const std_msgs::Float32 msg) {
-	float ticks = msg.data;
-	float currentDepth = (ticks-offset) * scalingFactor;
+	lastTicks = msg.data;
+	currentDepthRaw = (lastTicks-offset) * scalingFactor;
 	std_msgs::Float32 outMsg;
-	outMsg.data = currentDepth;
+	outMsg.data = currentDepthRaw;
 	depthPub.publish(outMsg);
+}
+
+void calibrationCallback(const std_msgs::Float32 msg) {
+	float depth = msg.data;
+	if(depth == 0) {
+		offset = lastTicks;
+	} else {
+		scalingFactor = depth / (lastTicks - offset);
+	}
 }
 
 int main(int argc, char** argv) {
 	ros::init(argc, argv, "DepthTranslator");
 	ros::NodeHandle nh;
 	ros::Subscriber curTicks = nh.subscribe("/Pressure_Data", 1, tickMeasureCallback);
+	ros::Subscriber calibrationSub = nh.subscribe("/Calibrate_Depth", 1, calibrationCallback);
 	depthPub = nh.advertise<std_msgs::Float32>("/Sub_Depth", 1);
 	ros::spin();
 }
