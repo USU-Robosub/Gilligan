7f5a7f41eaa4487fb6a13cb4095117e5381d8bd4
SubCrew <subcrew@SubLab2.(none)>
2012-05-17 17:03:14 -0600

    Added a simple center on point module in subSim

--- rosWorkspace/subSim/src/simpleDepth.cpp
+++ rosWorkspace/subSim/src/simpleDepth.cpp
@@ -1,12 +1,18 @@
 #include "ros/ros.h"
 #include "std_msgs/Float32.h"
 #include "motor.h"
+#include <Robosub/ModuleEnableMsg.h>
+
+#define OFF 0
+#define ON 1
 
 float targetDepth = 0;
 
 const int LEFT_MOTOR = 0x400;
 const int RIGHT_MOTOR = 0x200;
 const int REVERSED = 0x100;
+
+bool MODE = ON;
 
 ros::Publisher depthMotor;
 
@@ -34,7 +40,14 @@ void setDepthSpeed(float speed) {
 		);
 }
 
+void mEnabledCallback(const Robosub::ModuleEnableMsg::ConstPtr& msg) {
+	if(msg->Module == "Simple_Depth")
+		MODE = msg->State;
+}
+
 void mCurrentDepthCallback(const std_msgs::Float32::ConstPtr& msg) {
+	if(MODE == OFF)
+		return;
 	float depth = msg->data;
 	float speed = 0;
 	if(depth - targetDepth > 1.5)
@@ -51,5 +64,6 @@ int main(int argc, char** argv) {
 	ros::NodeHandle nh;
 	ros::Subscriber curDepth = nh.subscribe("Sub_Depth", 1, mCurrentDepthCallback);
 	ros::Subscriber targetDepth = nh.subscribe("/Target_Depth", 1, mTargetDepthCallback);
+	ros::Subscriber enabled = nh.subscribe("/Module_Control", 1, mEnabledCallback);
 	ros::spin();
 }
