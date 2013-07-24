d790922f01f8d0042709767dea631b1fb980d8b0
robosub <robosub@gilligan.(none)>
2013-07-24 13:50:56 -0600

    qualifytask tweaked slightly

--- rosWorkspace/QualifyTask/src/QualifyTask.py
+++ rosWorkspace/QualifyTask/src/QualifyTask.py
@@ -9,70 +9,74 @@ from SubMotorController.msg import MotorMessage
 from thread import start_new_thread
 
 from time import sleep
-
-taskThread = None;
-controlManualPub = None;
-depthPub = None;
-killSwitchSub = None;
-
-goForwardManualMsg = MotorMessage()
-
-def setup():
-    rospy.loginfo("Hello, qualifying...")
-    #Startup the node
-    rospy.init_node('diveAndDrive')
     
-    #Set up publishers
-    depthPub = rospy.Publisher('Simple_Depth', Float32)
-    controlManualPub = rospy.Publisher('Motor_Control', MotorMessage)
-    
-    #Set up Subscribers
-    killSwitchSub = rospy.Subscriber('Motor_State', UInt8, killSwitchCallback)
+class QualifyTask:
 
-    
-    goForwardManualMsg.mask = 3
-    
-
-
-    rospy.spin()
-    
-    
-def killSwitchCallback(motorState):
-	rospy.loginfo("Callback")
-    #if (motorState.data == 1):
-        #if not taskThread:
-            #taskThread = start_new_thread(diveAndDrive, tuple())
+    def __init__(self):
+        self.taskThread = None;
+        self.controlManualPub = None;
+        self.depthPub = None;
+        self.killSwitchSub = None;
         
-    #else:
-        #if taskThread:
-            #reset()
-
-def diveAndDrive():
-    #dive! :D
-    sleep(3)
-    depthPub.publish(Float32(3.0))
-    sleep(10)
-    
-    goForwardManualMsg.Left = 120
-    goForwardManualMsg.Right = 120
-    controlManualPub.publish(goForwardManualMsg)
-    
-    sleep(60)
-    start_new_thread(reset, tuple())
+        self.goForwardManualMsg = MotorMessage()
         
-def reset():
-    taskThread.exit()
-    taskThread = None;
-    
-    goForwardManualMsg.Left = 0
-    goForwardManualMsg.Right = 0    
-    controlManualPub.publish(goForwardManualMsg)
-    
-    depthPub.publish(Float32(3.0))
-    
+        rospy.loginfo("Hello, qualifying...")
+        #Startup the node
+        rospy.init_node('diveAndDrive')
+        
+        #Set up publishers
+        self.depthPub = rospy.Publisher('Target_Depth', Float32)
+        self.controlManualPub = rospy.Publisher('Motor_Control', MotorMessage)
+        
+        #Set up Subscribers
+        self.killSwitchSub = rospy.Subscriber('Motor_State', UInt8, self.killSwitchCallback)
+
+        
+        self.goForwardManualMsg.mask = 3
+        
+
+
+        rospy.spin()
+        
+        
+    def killSwitchCallback(self, motorState):
+        rospy.loginfo("Callback")
+        if (motorState.data == 1):
+            if not self.taskThread:
+                self.taskThread = start_new_thread(self.diveAndDrive, tuple())
+                rospy.loginfo("Starting qualify task")
+        else:
+            if self.taskThread:
+                self.reset()
+
+    def diveAndDrive(self):
+        #dive! :D
+        rospy.loginfo("diving...")
+        sleep(3)
+        self.depthPub.publish(Float32(5.0))
+        sleep(10)
+        rospy.loginfo("going foward...")
+        self.goForwardManualMsg.Left = 110
+        self.goForwardManualMsg.Right = 120
+        self.controlManualPub.publish(self.goForwardManualMsg)
+        
+        sleep(60)
+        start_new_thread(self.reset, tuple())
+            
+    def reset(self):
+        self.taskThread.exit()
+        self.taskThread = None;
+        
+        self.goForwardManualMsg.Left = 0
+        self.goForwardManualMsg.Right = 0
+        self.controlManualPub.publish(self.goForwardManualMsg)
+        
+        self.depthPub.publish(Float32(3.0))
+        
 if __name__ == '__main__':
     try:
-        setup()
+        rospy.loginfo("main")
+        qualify_task = QualifyTask()
     except rospy.ROSInterruptException:
         pass
     
